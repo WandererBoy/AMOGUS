575f42dca9cd840b47db361a95bfdc6f7100785b
WandererBoy <buzogluk@student.bmstu.ru>
2021-11-06 13:08:23 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int hui(int i){
+    i /= 6;
+    return i;
+}
+
+int main() {
+    int j;
+    cin >> j;
+    cout << hui(j);
+    return 0;
+}
+
+/* sdhjgljzio;fjl;kzjgkl;jz
+ ksjhbgfsaj
+ ]
+ \sl;kgjslkjfs
+ fsfsjogs
+ */
+
