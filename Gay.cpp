aa58c2cf530cff6e095a067c2bc2c34c1ef6a523
Alexandr Pravdin <pravdin.a.i@edu.mirea.ru>
2021-11-06 13:24:18 +0300

    Test

---
+++ Gay.cpp
@@ -0,0 +1,5 @@
+//
+// Created by iprav on 06.11.2021.
+//
+
+#include "Gay.h"
---
+++ Gay.h
@@ -0,0 +1,14 @@
+//
+// Created by iprav on 06.11.2021.
+//
+
+#ifndef UNTITLED_GAY_H
+#define UNTITLED_GAY_H
+
+
+class Gay {
+
+};
+
+
+#endif //UNTITLED_GAY_H
